16ec146afcb66dc857010d416c1fc6ef618b81f0
Mordechai <mordwein77@gmail.com>
2023-01-14 22:36:09 -0500

    commit tree 90642b938bd36d8b00b2414902ed76a04613a534

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+require "pry"
 
+class CashRegister
+    
+   attr_accessor :total, :discount 
+   
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        #binding.pry
+    end
+    def add_item(title, price, quantity = 1)
+        @price = price
+        @total += @price * quantity
+    end
+end
+ 
